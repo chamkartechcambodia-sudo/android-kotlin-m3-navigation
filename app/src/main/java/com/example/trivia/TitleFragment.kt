2fb5840446d0e4517a2facd41dd4f36e00360f2d
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-03-31 22:38:00 +0700

    feat: add TODOs ex06 Adding a Menu

--- app/src/main/java/com/example/trivia/TitleFragment.kt
+++ app/src/main/java/com/example/trivia/TitleFragment.kt
@@ -18,6 +18,19 @@ class TitleFragment : Fragment() {
           //  Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment2)
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment2)
         )
+        // TODO (02) Create the new menu resource
+        // Right click on the res folder within the Android project and select New Resource File
+        // Name it overflow_menu with resource type menu.  Add an About menu item with the ID of
+        // the aboutFragment.
+        // TODO (03) Call setHasOptionsMenu(true)
+        // This tells Android that our fragment has an Options Menu, so it will call
+        // onCreateOptionsMenu
         return binding.root
     }
+    // TODO (04) Override onCreateOptionsMenu
+    // Use the passed-in MenuInflater to inflate the overflow_menu
+
+    // TODO (05) Override onOptionsItemSelected
+    // Return true if NavigationUI.onNavDestinationSelected returns true, else return
+    // super.onOptionsItemSelected.
 }
